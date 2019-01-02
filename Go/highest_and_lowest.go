74c40ea20bee7709982ef86ab15a9b4d6cf04b51
dihernandez <diher@mit.edu>
2019-01-02 17:11:01 -0500

    Solved brackets kata

--- Go/highest_and_lowest.go
+++ Go/highest_and_lowest.go
@@ -1,19 +1,53 @@
 package kata
+import("bytes")
 
-import("strings"; "strconv")
 
-func HighAndLow(in string) string {
-  var split_string = strings.Split(in, " ")
-  max_num, _ := strconv.Atoi(split_string[0])
-  min_num, _ := strconv.Atoi(split_string[0])
-  for i := 0; i < len(split_string); i++ {
-    current_char, _ := strconv.Atoi(split_string[i]) 
-    if current_char < min_num {
-      min_num = current_char
-    }
-    if current_char > max_num {
-      max_num = current_char
-    }
+func ValidBraces(str string) bool {
+  paren_queue := make([]byte, 0)
+  brackets_queue := make([]byte, 0)
+  curly_queue := make([]byte, 0)
+
+  if str[0] == ')' || str[0] == ']' || str[0] == '}' {
+    return false
   }
-  return strconv.Itoa(max_num) + " " + strconv.Itoa(min_num)
+  
+  for c := 0; c < len(str) - 1; c++ {
+    if (str[c] == '(' && str[c + 1] == '}') || (str[c] == '(' && str[c + 1] == ']') {
+      return false
+    } 
+    if (str[c] == '[' && str[c + 1] == '}') || (str[c] == '[' && str[c + 1] == ')') {
+      return false
+    } 
+    if (str[c] == '{' && str[c + 1] == ']') || (str[c] == '{' && str[c + 1] == ')') {
+      return false
+    } 
+  }
+    for c := 0; c < len(str); c++ {
+      if str[c] == '(' {
+        paren_queue = append(paren_queue, str[c])
+      } else if str[c] == ')' {
+        if bytes.Contains(paren_queue, []byte("(")) {
+          paren_queue = paren_queue[:len(paren_queue) - 1]
+        } else {
+          paren_queue = append(paren_queue, str[c])
+        }
+      } else if str[c] == '[' {
+        brackets_queue = append(brackets_queue, str[c])
+      } else if str[c] == ']'{
+        if bytes.Contains(brackets_queue, []byte("[")) {
+            brackets_queue = brackets_queue[:len(brackets_queue) - 1]
+          } else {
+            brackets_queue = append(brackets_queue, str[c])
+          }
+      } else if str[c] == '{' {
+        curly_queue = append(curly_queue, str[c])
+      } else if str[c] == '}' {
+        if bytes.Contains(curly_queue, []byte("{")){
+          curly_queue = curly_queue[:len(curly_queue) - 1]
+        } else {
+        curly_queue = append(curly_queue, str[c])
+        }
+      }
+  }
+  return len(paren_queue) + len(brackets_queue) + len(curly_queue) == 0
 }
