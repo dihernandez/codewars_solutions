936818fdf9d6a085c4c7be2a25b94a651acf0885
Diana Hernandez <diher@mit.edu>
2020-10-12 17:46:35 -0400

    have mostly functional sample tests, about to overhaul system

--- CPP/snail.cpp
+++ CPP/snail.cpp
@@ -8,19 +8,28 @@ using namespace std;
 std::vector<int> snail(const std::vector<std::vector<int>> &snail_map) {
   vector<int> result {};
   enum direction {RIGHT, DOWN, LEFT, UP} dir;
-
+  
   dir = RIGHT;
   if(snail_map.size() == 0){ 
     return {};
+  } else if(snail_map.size() == 1){
+    return snail_map[0];
   }
-  
+;
+
   int length = snail_map.size(); // initial length and deapth
   int deapth = snail_map[0].size();
+  int start_deapth = 0;
+  int start_length = 0;
+  int from_right = 0;
+  int from_left = 0;
+  int from_top = 0;
+  int from_bottom = 0;
   
   int start = snail_map[0][0];
     
   int test = 0;
-  while(test < 10) {  //result.size() < length*deapth
+  while(result.size() <= 4*length*deapth){
     test++;
     switch (dir) {
         case RIGHT:        
@@ -30,10 +39,11 @@ std::vector<int> snail(const std::vector<std::vector<int>> &snail_map) {
                     cout << "changing direction " << endl;
                     dir = DOWN;
                     length -= 1;
+                    from_top++;
                     break;
                 } else {
-                cout << "pushing back " << " i is " << i << "deapth - 1 " << snail_map[0][i] << endl;
-                result.push_back(snail_map[0][i]);
+                cout << "pushing back " << " i is " << i << " from_top is " << from_top << " " << snail_map[from_top][i] << endl;
+                result.push_back(snail_map[from_top][i]);
                 }
 
             }
@@ -41,28 +51,33 @@ std::vector<int> snail(const std::vector<std::vector<int>> &snail_map) {
 
         case DOWN:
             cout <<" going down "<< endl;
-            for(int i = 0; i <= deapth; i++){
+            for(int i = 1; i <= deapth; i++){
                 if(i== deapth) {
+                    cout << "in i == deapth" << endl;
                     dir = LEFT;
                     deapth -= 1;
+                    start_length++;
                     break;
                 } else {
-                    cout << "pushing back " << " i is " << i << " " << snail_map[i][0] << endl;
-                    result.push_back(snail_map[i][0]);
+                    cout << "pushing back " << " i is " << i << " " << snail_map[i][length] << "deapth is " << deapth << endl;
+                    result.push_back(snail_map[i][length]);
                 }
             }
             break;
 
         case LEFT:
             cout <<" going left "<< endl;
-                for(int i = length; i >= 0; i--){
-
+                for(int i = length - 1; i >= 0; i--){
+                
+                cout << "pushing back " << " i is " << i << " " << snail_map[deapth][i] << " deapth " << deapth << endl;
+                result.push_back(snail_map[deapth][i]);
+            
                 if(i== 0) {
                     dir = UP;
                     length -= 1;
+                    start_deapth++;
                     break;
                 }
-                result.push_back(snail_map[i][0]);
             }
             break;
 
@@ -73,9 +88,14 @@ std::vector<int> snail(const std::vector<std::vector<int>> &snail_map) {
                 if(i== 0) {
                     dir = RIGHT;
                     deapth -= 1;
+                    from_right++;
                     break;
                 }
-                result.push_back(snail_map[0][i]);
+
+                cout << "pushing back " << " i is " << i << " snail map selection is " << snail_map[i][from_right] << " from_right " << from_right << endl;
+                result.push_back(snail_map[i - 1][from_right]);
+                
+
             }
             break;
     }
@@ -85,7 +105,7 @@ std::vector<int> snail(const std::vector<std::vector<int>> &snail_map) {
 }
 
 int main(){
-    vector<int> result =  snail({{1,2}, {4,3}});
+    vector<int> result =  snail({{1,2,3,4}, {12,13,14,5}, {11,16,15,6}, {10,9,8,7}});
     
     for(auto v : result) {
         cout << v << endl;
