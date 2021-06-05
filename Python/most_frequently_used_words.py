e0ed79e9975f05c98f8ba6d3100dac8240a714d7
Diana Hernandez <diher@mit.edu>
2021-06-05 18:57:52 -0400

    cleaned up print statements

--- Python/most_frequently_used_words.py
+++ Python/most_frequently_used_words.py
@@ -14,20 +14,11 @@ def is_not_just_ticks(word):
 def top_3_words(text):
     word_occurences = {}
     clean_text = []
-    #r = re.compile(r'[^\P{P}\']+'.format("'" + re.escape(punctuation)))
-    #words_list = re.split(ur"[^\P{P}']+", text)
-    # remove = punctuation
-    # remove = remove.replace("'", "")
-    #remove += " "
     r = re.compile(r'[^\w\'\_\s]|_')
-    #r = re.compile(remove)
     words_list = r.split(text)
-    #words_list = r.split(text)
-    #words_list = re.split(remove, text)
     words_list_split_by_space = []
     for word in words_list:
         words_list_split_by_space.extend(word.split())
-    #print("words list is \n ", words_list)
     for word in words_list_split_by_space:
         clean_word = ""
         for character in word:
@@ -39,7 +30,6 @@ def top_3_words(text):
     for word in clean_text:
         if is_not_just_ticks(word):
             no_ticks_words_list.append(word)
-    #print(no_ticks_words_list)
     no_empty_words_list = filter(lambda x: x != '', no_ticks_words_list)
     for word in no_empty_words_list:
         if word in word_occurences.keys():
@@ -57,5 +47,5 @@ def top_3_words(text):
     else:
         return [word[0] for word in sorted_words[0:3]]
 
-#TODO(diher) exlcude ' from punctuation' 
+
 print(top_3_words("KxxDzL!//uvzpqmZOyE-; -KxxDzL /KxxDzL/!,?KxxDzL_KxxDzL;_?KxxDzL:KxxDzL /:,/KxxDzL;KxxDzL , :.KxxDzL!-!,.KxxDzL/KxxDzL:,KxxDzL ?uvzpqmZOyE-!;:"))
